57af53435c18ce54bd6073f0267bfd8883ee18fa
Riccardo <r.sannipoli@lineargruppo.it>
2023-03-24 18:02:14 +0100

    fix routing navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {Switch, Route, Router} from "react-router-dom";
 import { StylesProvider } from "@material-ui/core";
 
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
-export default () => {
+export default ({history}) => {
     return <>
-        <StylesProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path="/" component={Landing} />
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+        <div>
+            <StylesProvider>
+                <Router history={history}>
+                    <Switch>
+                        <Route exact path="/pricing" component={Pricing} />
+                        <Route path="/" component={Landing} />
+                    </Switch>
+                </Router>
+            </StylesProvider>
+        </div>
     </>;
 }
